3ddf8817033b08634d68bc0f8cbbfa84b0107a94
Jon Schulman <schulman99@gmail.com>
2018-06-25 22:09:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
-  # if array.size <= 2
-  #   array.join(" and ")
-  # else
-  # last_element = array.pop
-  # return "#{array.join(", ")}, and #{last_element}"
-  array.to_sentence
+  if array.size <= 2
+    array.join(" and ")
+  else
+  last_element = array.pop
+  return "#{array.join(", ")}, and #{last_element}"
 end
 end
